017ea146e11c478882d5b32a14adb86e2bbda769
Molly <57105640+Bumbumkill@users.noreply.github.com>
2020-03-09 15:00:23 +0700

    Update Main.php

--- src/Bumbumkill/ShulkerBox/Main.php
+++ src/Bumbumkill/ShulkerBox/Main.php
@@ -3,10 +3,28 @@
 namespace Bumbumkill\ShulkerBox;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use Bumbumkill\ShulkerBox\tile\ShulkerBox as ShulkerTile;
+use pocketmine\tile\Tile;
+use pocketmine\block\Block;
+use pocketmine\block\BlockFactory;
+use pocketmine\item\Item;
+use pocketmine\utils\Config;
+use Bumbumkill\ShulkerBox\block\ShulkerBox;
 
-class Main extends PluginBase {
+class Main extends PluginBase implements Listener {
+
+public static $shulkerEnable = true;
 
 public function onEnable(){
-    	$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	Tile::registerTile(ShulkerTile::class);
+	BlockFactory::registerBlock(new ShulkerBox(Block::UNDYED_SHULKER_BOX), true);
+	BlockFactory::registerBlock(new ShulkerBox(), true);
+	Item::initCreativeItems();
+	@mkdir($this->getDataFolder());
+        $this->saveResource("config.yml");
+        $config = new Config($this->getDataFolder() . 'config.yml', Config::YAML);
+        $shulkerEnable = $config->getNested("Function.enable");
+	$this->getServer()->getPluginManager()->registerEvents($this, $this);
   }
 }
